0843b608ec57dacdbea12f349001e7a5c4c3a968
Ariel Ladner <155122182+anladner-clone@users.noreply.github.com>
2024-10-01 10:34:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 import pandas as pd
 
 # Leer el archivo Excel
-df = pd.read_excel('ruta_del_archivo.xlsx')
-
-# Mostrar las primeras filas para verificar los datos
-print(df.head())
-
-import matplotlib.pyplot as plt
+df = pd.read_excel('python\Registros.xlsx')
 
 # Contar cuántos compraron offline y online
 canal_compras = df['Canal_de_venta'].value_counts()
 
-# Crear el gráfico de barras
-canal_compras.plot(kind='bar', color=['blue', 'orange'])
-plt.title('Cantidad de Compras Offline vs Online')
-plt.xlabel('Canal de Venta')
-plt.ylabel('Cantidad de Personas')
-plt.show()
+print(canal_compras)
 
-# Agrupar por cliente y sumar el importe total de las compras
-clientes_top = df.groupby('ID_Cliente')['Importe_Coste_total'].sum()
+tipo_de_producto = df['Tipo de producto'].value_counts()
 
-# Ordenar los clientes de mayor a menor
-clientes_top = clientes_top.sort_values(ascending=False).head(10)  # Los 10 que más compraron
+print(tipo_de_producto)
 
-# Crear el gráfico de barras
-clientes_top.plot(kind='bar', color='green')
-plt.title('Top 10 Clientes que más Compraron')
-plt.xlabel('ID del Cliente')
-plt.ylabel('Total Gastado')
-plt.show()
+clientes_top = df.groupby(['ID_Cliente','Zona'])['Importe_Coste_total'].sum()
+clientes_top = clientes_top.sort_values(ascending=False).head(10)
+print(clientes_top)
